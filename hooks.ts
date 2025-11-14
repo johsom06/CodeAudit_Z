aa131a18c3991e5dbbb6d317c36d7bcd7b017869
johsom06 <johsom06@users.noreply.github.com>
2025-11-14 09:03:00 +0000

    Add vite.config.ts, hardhat.config.ts, hooks.ts, layouts.jsx, local-utils.ini, mutations-export.tsx

---
+++ hooks.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+export const constants_middleware = () => { return null; };
+
---
+++ mutations-export.tsx
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+interface fragments_message_middlewareProps { data: string }
+
